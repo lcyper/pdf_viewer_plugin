9eedcc37a4efb2918a92d5eb1bc236a9f561c749
lcyper <lcyper@gmail.com>
2022-02-15 13:55:19 -0300

    update to new versions imports of plugin android

--- example/android/app/src/main/java/dev/britto/pdf_viewer_plugin_example/MainActivity.java
+++ example/android/app/src/main/java/dev/britto/pdf_viewer_plugin_example/MainActivity.java
@@ -2,5 +2,14 @@ package dev.britto.pdf_viewer_plugin_example;
 
 import io.flutter.embedding.android.FlutterActivity;
 
+
+
 public class MainActivity extends FlutterActivity {
 }
+
+// startActivity(
+//   MainActivity
+//     .withNewEngine()
+//     .backgroundMode(FlutterActivityLaunchConfigs.BackgroundMode.transparent)
+//     .build(context)
+// );
